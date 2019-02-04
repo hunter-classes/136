4ac4e97459cfa8fb78baee508da7e2b5fe52154a
Mike Zamansky <zamansky@gmail.com>
2019-02-04 09:24:37 -0500

    added include file

---
+++ classcode/multi-files/includes/funcs.h
@@ -0,0 +1,11 @@
+#ifndef FUNCS_H
+#define FUNCS_H
+
+// instead of the three #ifndef, #define, and #endif we can
+// just put #pragma once at the top
+
+int add2(int, int);
+int add3(int, int, int);
+
+#endif
+
