985d7b8fd332108449ca6a3d288fbbc40ae27696
Mike Zamansky <zamansky@gmail.com>
2019-02-11 09:25:00 -0500

    added simple make example

---
+++ classcode/make/simple/funcs.cpp
@@ -0,0 +1,14 @@
+
+#include "funcs.h"
+
+int add3(int a, int b, int c){
+  return add2(a,b)+c;
+}
+
+
+int add2(int a, int b){
+  return a+b;
+}
+
+
+
---
+++ classcode/make/simple/funcs.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// instead of the three #ifndef, #define, and #endif we can
+// just put #pragma once at the top
+
+int add2(int, int);
+int add3(int, int, int);
+
