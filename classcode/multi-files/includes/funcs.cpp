34d1352f91503465e00d9f84f23c4fe56b74f887
Mike Zamansky <zamansky@gmail.com>
2019-02-11 08:42:47 -0500

    finishde with multi files

--- classcode/multi-files/includes/funcs.cpp
+++ classcode/multi-files/includes/funcs.cpp
@@ -1,6 +1,5 @@
 
 #include "funcs.h"
-#include "funcs.h"
 
 int add3(int a, int b, int c){
   return add2(a,b)+c;
--- classcode/multi-files/includes/funcs.h
+++ classcode/multi-files/includes/funcs.h
@@ -1,11 +1,8 @@
-#ifndef FUNCS_H
-#define FUNCS_H
+#pragma once
 
 // instead of the three #ifndef, #define, and #endif we can
 // just put #pragma once at the top
 
 int add2(int, int);
 int add3(int, int, int);
-
-#endif
 
