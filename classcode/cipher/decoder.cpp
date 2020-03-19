a7895c8c3832a2bcd2237eb62ed3e5ff40107854
Mike Zamansky <zamansky@gmail.com>
2020-03-19 10:53:35 -0400

    added decoder.cpp

--- classcode/cipher/decoder.cpp
+++ classcode/cipher/decoder.cpp
@@ -5,7 +5,8 @@
 std::string encode(std::string s, int r){
   char c;
   std::string result="";
-  for (int i ; i < s.length(); ++i) {
+  /*  I forgot to set i to 0 in the loop so it just kept it's value from before!!!! */
+  for (int i=0 ; i < s.length(); ++i) {
     c = s[i];
     if (c >= 'a' && c<='z'){
       c  = c - 'a';
@@ -18,16 +19,24 @@ std::string encode(std::string s, int r){
       c = c + 'A';
     }
     result = result + c;
-  }
+  } 
   return result;
 }
 
+void stupid_decoder(std::string s){
+  int i;
+  std::string decoded;
+  for (i = 0; i < 26; ++i) {
+    decoded = encode(s,i);
+    std::cout << i << " : " <<  encode(s,i) << "\n";
+  }
+}
 
 int main()
 {
-  std::string s = "hello world";
-  std::string result = encode(s,3);
-  std::cout << s << " : " << result << "\n";
-  
+  int i;
+  std::string s = "gzzgiq kgin jge cozn gt ktznayogys atqtuct zu sgtqotj";
+  stupid_decoder(s);
+
   return 0;
 }
